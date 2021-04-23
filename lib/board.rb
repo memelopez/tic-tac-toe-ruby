98c220e3ee14c3d3edbcbd698794c234d7bdd48c
Elmer Lopez <meme_lopez10@hotmail.com>
2021-04-23 07:57:27 -0400

    Fix offenses

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+class Board
+  attr_reader :cells, :plays_count
+
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @plays_count = 0
+  end
+
+  def print_board
+    %(+ --- + --- + --- +
+  #{@cells[0]}   |  #{@cells[1]}  |  #{@cells[2]}
++ --- + --- + --- +
+  #{@cells[3]}   |  #{@cells[4]}  |  #{@cells[5]}
++ --- + --- + --- +
+  #{@cells[6]}   |  #{@cells[7]}  |  #{@cells[8]}
++ --- + --- + --- +
+    )
+  end
+
+  def update_cell(index, value)
+    @cells[index.to_i - 1] = value
+    @plays_count += 1
+  end
+end
---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+class Player
+  attr_reader :name, :mark
+  attr_accessor :plays
+
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+    @plays = []
+  end
+
+  def plyr2string
+    %(Player #{@name} marks #{@mark} plays: #{@plays})
+  end
+end
