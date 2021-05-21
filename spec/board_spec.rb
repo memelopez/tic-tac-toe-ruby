5c44aa51c3b43764107b28e525c9119d91a8bcfe
Elmer Lopez <meme_lopez10@hotmail.com>
2021-05-20 20:54:41 -0600

    Add context, truthy, and falsey tests for board_spec and player_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,8 +6,9 @@ describe Board do
   let(:board) { Board.new }
 
   describe '#print_board' do
-    it 'returns a string that represents the board' do
-      expect(board.print_board).to eql(%(+ --- + --- + --- +
+    context 'when printing the board' do
+      it 'returns a string that represents the board' do
+        expect(board.print_board).to eql(%(+ --- + --- + --- +
    #{board.cells[0]}  |  #{board.cells[1]}  |  #{board.cells[2]}
 + --- + --- + --- +
    #{board.cells[3]}  |  #{board.cells[4]}  |  #{board.cells[5]}
@@ -15,18 +16,27 @@ describe Board do
    #{board.cells[6]}  |  #{board.cells[7]}  |  #{board.cells[8]}
 + --- + --- + --- +
     ))
+      end
     end
   end
 
   describe '#update_cell' do
-    it "asigns a value to board's array" do
-      expect(board.update_cell(1, 'X')).to eql('X')
+    context 'when marking a cel in the board' do
+      it "asigns a value to board's array" do
+        expect(board.update_cell(1, 'X')).to eql('X')
+      end
     end
   end
 
   describe '#free_cell?' do
-    it 'returns a booean value if a certain cell is available' do
-      expect(board.free_cell?(1)).to be(true).or be(false)
+    context 'when verifying if a cell is available' do
+      it 'returns true when a cell is available' do
+        expect(board.free_cell?(1)).to be_truthy
+      end
+      it 'returns false when a certain cell is already marked' do
+        board.update_cell(1, 'X')
+        expect(board.free_cell?(1)).to be_falsey
+      end
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,14 +6,18 @@ describe Player do
   let(:plyr1) { Player.new('Leo Messi', 'X') }
 
   describe '#plyr2string' do
-    it 'returns a string with the information of the player' do
-      expect(plyr1.plyr2string).to eql('Player :: Leo Messi -- marks :: X -- plays: [] --<< ')
+    context 'when asking a player for his info' do
+      it "returns a string with player's name and mark" do
+        expect(plyr1.plyr2string).to eql('Player :: Leo Messi -- marks :: X -- plays: [] --<< ')
+      end
     end
   end
 
   describe '#add_play' do
-    it 'returns an array with the positions the player has marked' do
-      expect(plyr1.add_play(1)).to eql([0])
+    context 'when adding a position that player marked' do
+      it 'returns an array with the positions the player has marked' do
+        expect(plyr1.add_play(1)).to eql([0])
+      end
     end
   end
 end
